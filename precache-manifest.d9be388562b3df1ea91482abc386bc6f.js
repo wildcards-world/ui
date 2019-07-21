0a933ebf51d60ac358bca60e8647d79aa2ca7d69
Jason Smythe <Jason@smythemail.za.net>
2019-07-21 23:58:06 +0200

    Updates

---
+++ precache-manifest.d9be388562b3df1ea91482abc386bc6f.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "910663892c3deaad8e878103f2ed92f9",
+    "url": "/index.html"
+  },
+  {
+    "revision": "f9b1f50cd2cd0f386ab0",
+    "url": "/static/js/2.08084da9.chunk.js"
+  },
+  {
+    "revision": "05b609523daa3e464808",
+    "url": "/static/js/main.aa754c2d.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "d41fa74966b767907b42b1eb0712ec7c",
+    "url": "/static/media/gorilla1.d41fa749.png"
+  },
+  {
+    "revision": "d1afd4bec5e1daeed0f7d734826d0259",
+    "url": "/static/media/gorilla2.d1afd4be.png"
+  },
+  {
+    "revision": "4eb0db0de112e72294754c8a26140fb2",
+    "url": "/static/media/gorilla3.4eb0db0d.png"
+  },
+  {
+    "revision": "a59c97f611aac33bd0bbb06e6f1fd96b",
+    "url": "/static/media/wild-cards-small.a59c97f6.png"
+  }
+]);
