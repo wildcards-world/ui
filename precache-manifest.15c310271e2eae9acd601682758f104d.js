86536a340c83e0e06d58f097d20f5e7892e0c110
Jason Smythe <jason@smythemail.za.net>
2019-09-29 19:03:49 +0200

    Updates

---
+++ precache-manifest.15c310271e2eae9acd601682758f104d.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2ce08813c5a20011129ba49d9c2cbee1",
+    "url": "./index.html"
+  },
+  {
+    "revision": "ced0763cb558fe7b82b6",
+    "url": "./static/js/2.a43c41f9.chunk.js"
+  },
+  {
+    "revision": "88d6110c08ea49d44eb4",
+    "url": "./static/js/main.3793b5a9.chunk.js"
+  },
+  {
+    "revision": "c58c18cf7e58ec946352",
+    "url": "./static/js/runtime-main.c3289c7e.js"
+  },
+  {
+    "revision": "c448e5356953e6cff18c00a20ab4ecd8",
+    "url": "./static/media/gorilla-on-vine.c448e535.png"
+  },
+  {
+    "revision": "d41fa74966b767907b42b1eb0712ec7c",
+    "url": "./static/media/gorilla1.d41fa749.png"
+  },
+  {
+    "revision": "d1afd4bec5e1daeed0f7d734826d0259",
+    "url": "./static/media/gorilla2.d1afd4be.png"
+  },
+  {
+    "revision": "4eb0db0de112e72294754c8a26140fb2",
+    "url": "./static/media/gorilla3.4eb0db0d.png"
+  },
+  {
+    "revision": "28738cf6e8a6d36015ae224b10e000d0",
+    "url": "./static/media/hanging-gorilla-from-vine.28738cf6.png"
+  },
+  {
+    "revision": "9351fd92471d5aebf9464819ab40916a",
+    "url": "./static/media/instagram.9351fd92.svg"
+  },
+  {
+    "revision": "9d92bf945a43784fc831648a73714058",
+    "url": "./static/media/raising_hands.9d92bf94.png"
+  },
+  {
+    "revision": "358d6cce5afddebf8bb8507be5c9466b",
+    "url": "./static/media/twitter.358d6cce.svg"
+  },
+  {
+    "revision": "a59c97f611aac33bd0bbb06e6f1fd96b",
+    "url": "./static/media/wild-cards-small.a59c97f6.png"
+  },
+  {
+    "revision": "ae5d91e259c50b670869803381cee1bc",
+    "url": "./static/media/wild-jungle-background.ae5d91e2.png"
+  }
+]);
