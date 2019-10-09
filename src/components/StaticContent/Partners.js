57d4ea58f7710344dcddbb595dcfbe2a2ed1672d
denhampreen <denhampreen@gmail.com>
2019-10-09 13:53:45 +0200

    cards for partners section

--- src/components/StaticContent/Partners.js
+++ src/components/StaticContent/Partners.js
@@ -1,4 +1,26 @@
 import React from "react"
-export default () => <div>
-  hello world
+import { Flex, Box, Card, Heading, Text } from 'rimble-ui';
+import { infoBackground } from '../../Styles.bs'
+
+const Partners = () => <div>
+  <Flex styles={infoBackground}>
+    <Box p={3} width={1 / 3} color="black">
+      <Card>
+        
+        <Heading>Ubisoft</Heading>
+        <Text>
+          Incubator program
+        </Text>
+      </Card>
+    </Box>
+    <Box p={3} width={1 / 3} color="black">
+      <Card>EthCapeTown</Card>
+
+    </Box>
+    <Box p={3} width={1 / 3} color="black">
+      <Card>Wild Tommorrow Fund</Card>
+    </Box>
+  </Flex>
 </div>
+
+export default Partners
