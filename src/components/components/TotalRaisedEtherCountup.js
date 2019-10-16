b31b679379d28e32c5a5b538c1bb44ef87bf599a
jonjon <jonjonclark@gmail.com>
2019-10-16 11:55:12 +0200

    CountUp hooks slight mod

--- src/components/components/TotalRaisedEtherCountup.js
+++ src/components/components/TotalRaisedEtherCountup.js
@@ -1,25 +1,33 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import { useCountUp } from "react-countup";
 
 export default ({ totalRaised }) => {
-  var res = totalRaised.split(".");
+  const [res, setRes] = useState(totalRaised.split("."));
+
+  useEffect(() => {
+    setRes(totalRaised.split("."));
+    console.log(totalRaised);
+  }, [totalRaised]);
 
   const { countUp, start, pauseResume, reset, update } = useCountUp({
     start: 783705291160578410,
-    end: 783708145041856818,
+    end: res[1],
     delay: 1000,
     duration: 2,
     onReset: () => console.log("Resetted!"),
     onUpdate: () => console.log("Updated!"),
     onPauseResume: () => console.log("Paused or resumed!"),
-    onStart: ({ pauseResume }) => console.log(pauseResume),
-    onEnd: ({ pauseResume }) => console.log(pauseResume)
+    onStart: ({ pauseResume }) => console.log("pauseResume"),
+    onEnd: ({ pauseResume }) => console.log("pauseResume")
   });
+
+  const counterup = useCountUp({ end: res[1] });
 
   return (
     <div>
       <div>
         {res[0]}.{countUp}
+        {res[0]}.{counterup}
       </div>
       <button onClick={start}>Start</button>
       <button onClick={reset}>Reset</button>
