7b6e085a6d5a1cf6cca5deb0d5e15cb044703deb
Jason Smythe <jason@smythemail.za.net>
2019-10-16 16:13:49 +0200

    Swap two input boxes

--- src/components/BuyModelInput.js
+++ src/components/BuyModelInput.js
@@ -35,59 +35,59 @@ export default ({
     <Box p={2} mb={2} >
       <Heading>Purchase {gorillaName}</Heading>
     </Box>
-    </Flex >
-    <Flex >
-    <Box p={2} mb={2} width={[1, 0.5]}>
-      <Text>
-        Set your monthly contribution:
-      </Text>
-      <Input
-        type="number"
-        placeholder="Your Initial Sale Price"
-        onChange={eventToValue(setNewPrice)}
-        value={newPrice}
-      />
-    </Box>
-    <Box p={2} mb={2} width={[1, 0.5]}>
-      <Text>
-        This will be {gorillaName}'s new for sale price:
-      </Text>
-      <Input
-        type="number"
-        placeholder="Monthly Patronage"
-        onChange={eventToValue(updatePatronage)}
-        value={patronage}
-      />
-    </Box>
-    </Flex >
-    <Flex >
-    <Box p={2} mb={2} width={[1, 0.5]}>
-      <Input
-        type="number"
-        placeholder="Your Initial Deposit"
-        onChange={eventToValue(setDeposit)}
-        value={deposit}
-      />
-    </Box>
-          </Flex >
-          <Flex >
-    <Box p={2} mb={2} width={[1, 0.5]}>
-      <Slider value={depositSlider}
-        onChange={
-          event => {
-            let value = event.target.value
-            setDepositSlider(value)
-            debouncedSetDeposit(value)
-          }
-        }
-        min={"0.0000001"} max={maxAvailableDeposit} step={"0.0000001"} />
-      <p>This deposit will last {depositTimeInSeconds} seconds</p>
-      <p>This deposit will last {depositTimeInSeconds / 2628000} months</p>
-      <br />
-      <Button onClick={onSubmitBuy}>
-        Buy
-    </Button>
-    </Box>
   </Flex >
+    <Flex >
+      <Box p={2} mb={2} width={[1, 0.5]}>
+        <Text>
+          This will be {gorillaName}'s new for sale price:
+      </Text>
+        <Input
+          type="number"
+          placeholder="Monthly Patronage"
+          onChange={eventToValue(updatePatronage)}
+          value={patronage}
+        />
+      </Box>
+      <Box p={2} mb={2} width={[1, 0.5]}>
+        <Text>
+          Set your monthly contribution:
+      </Text>
+        <Input
+          type="number"
+          placeholder="Your Initial Sale Price"
+          onChange={eventToValue(setNewPrice)}
+          value={newPrice}
+        />
+      </Box>
+    </Flex >
+    <Flex >
+      <Box p={2} mb={2} width={[1, 0.5]}>
+        <Input
+          type="number"
+          placeholder="Your Initial Deposit"
+          onChange={eventToValue(setDeposit)}
+          value={deposit}
+        />
+      </Box>
+    </Flex >
+    <Flex >
+      <Box p={2} mb={2} width={[1, 0.5]}>
+        <Slider value={depositSlider}
+          onChange={
+            event => {
+              let value = event.target.value
+              setDepositSlider(value)
+              debouncedSetDeposit(value)
+            }
+          }
+          min={"0.0000001"} max={maxAvailableDeposit} step={"0.0000001"} />
+        <p>This deposit will last {depositTimeInSeconds} seconds</p>
+        <p>This deposit will last {depositTimeInSeconds / 2628000} months</p>
+        <br />
+        <Button onClick={onSubmitBuy}>
+          Buy
+    </Button>
+      </Box>
+    </Flex >
   </Fragment>
 }; 
