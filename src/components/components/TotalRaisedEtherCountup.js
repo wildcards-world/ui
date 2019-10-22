bccb44090c2a1d508241b39ddc6da194accdecea
Jason Smythe <jason@smythemail.za.net>
2019-10-22 13:07:38 +0200

    Cleanup unnecessary code from countup

--- src/components/components/TotalRaisedEtherCountup.js
+++ src/components/components/TotalRaisedEtherCountup.js
@@ -1,65 +1,45 @@
-import React, { useState, useEffect, useRef } from "react";
-import { useCountUp } from "react-countup";
+import React, { useState, useEffect, useRef } from "react"
+import { useCountUp } from "react-countup"
 
-const Counter = ({ from, till }) => {
-  const ref = useRef(null);
+export default ({ totalRaised }) => {
+  const res = totalRaised.split(".")
+  const decimals = parseInt(res[1])
+
   const [styles, setStyles] = useState({})
+  const ref = useRef(0);
   const { countUp, start, pauseResume, reset, update } = useCountUp({
-    start: till,
-    end: till,
+    start: decimals,
+    end: decimals,
     delay: 1000,
     duration: 6,
     redraw: false,
     preserveValue: true, // This line is extremely important, otherwise it always starts from zero!!
-    // onReset: () => console.log("Resetted!"),
-    // onUpdate: () => console.log("Updated!"),
-    // onPauseResume: () => console.log("Paused or resumed!"),
-    // onStart: ({ pauseResume }) => console.log("pauseResume"),
-    // onEnd: ({ pauseResume }) => console.log("pauseResume")
   });
 
   useEffect(() => {
-    console.log({ from, till })
     let timeout
-    if (ref.current !== till) {
-      ref.current = till;
-      if (from === 0) {
-        start()
+    if (ref.current !== decimals) {
+
+      ref.current = decimals
+      if (ref.current === 0) {
+        if (decimals !== 0) {
+          start()
+        }
       } else {
-        update(till)
+        update(decimals)
         setStyles({ color: "green", fontWeight: "bold" })
         timeout = setTimeout(() => setStyles({}), 900)
       }
     }
     return () => clearTimeout(timeout)
-  }, [till])
-
-  return (
-    <span style={styles}>
-      {countUp}
-    </span >
-  );
-};
-
-export default ({ totalRaised }) => {
-  const res = totalRaised.split(".");
-
-  const refPrev = useRef(0);
-  const ref = useRef(0);
-  // const key = useRef(0);
-
-  useEffect(() => {
-    if (ref.current !== parseInt(res[1]))
-      // key.current = key.current + 1
-
-      // refPrev.current = ref.current;
-      ref.current = parseInt(res[1]);
   }, [totalRaised])
 
 
   return (
     <React.Fragment>
-      {res[0]}.<Counter from={ref.current} till={parseInt(res[1])} />
+      <span style={styles}>
+        {res[0]}.{countUp}
+      </span >
     </React.Fragment>
   );
 };
