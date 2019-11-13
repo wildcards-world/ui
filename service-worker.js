e217e6c1338c4797fe4a73b8a621a902e71354fc
Jason Smythe <jason@smythemail.za.net>
2019-11-13 21:05:25 +0100

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "./precache-manifest.f2071dcec8c28a3656866f85173cca8a.js"
+  "./precache-manifest.27fb16fbfe5a22f9b964903e29d98af3.js"
 );
 
 self.addEventListener('message', (event) => {
