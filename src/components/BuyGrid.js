9021422ad3486841dc7498c7bc733aa9b6f0b5ad
denhampreen <denhampreen@gmail.com>
2019-12-04 23:00:16 +0100

    Grid form, need to edit animal inputs

--- src/components/BuyGrid.js
+++ src/components/BuyGrid.js
@@ -1,17 +1,45 @@
-import React, { Fragment } from "react"
+import React, { Fragment } from "react";
+import { Card, Box, Flex } from "rimble-ui";
 
-export default ({
-  animalArray
-}) => {
-  return <Fragment>
-    <p>available animals</p>
-    {animalArray.map((Animal, key) => <div key={key} style={{
-      maxWidth: "150px",
-      textAlign: "center"
-    }}>
-      <  Animal />
-    </div>
-    )}
-  </Fragment >
+const styles = {
+  background: {
+    paddingTop: "1rem",
+    backgroundImage: "linear-gradient(to top, #448661 0%, #74C7D7 100%)"
+  },
+  center: {
+    textAlign: "center"
+  }
 };
 
+export default ({ animalArray }) => {
+  return (
+    <div style={styles.background}>
+      <div>
+        <h1 style={{ paddingTop: "5rem", textAlign: "center" }}>
+          Wildcards Animal Marketplace
+        </h1>
+      </div>
+      <Flex
+        flexWrap={"wrap"}
+        justifyContent={"space-around"}
+        alignItems={"stretch"}
+        px={50}
+      >
+        {animalArray.map((Animal, key) => (
+          <Box
+            key={key}
+            // bg="salmon"
+            color="white"
+            fontSize={4}
+            p={3}
+            width={[1, 1, 0.3]}
+          >
+            <Card>
+              <Animal />
+            </Card>
+          </Box>
+        ))}
+      </Flex>
+    </div>
+  );
+};
