230151ac5a5e26e54b28bd73d5269dd3af2bf140
JasoonS <JasoonS@users.noreply.github.com>
2020-04-29 14:55:40 +0000

    Automated deployment: Wed Apr 29 14:55:40 UTC 2020 f8c7c479cc47a150163ed10d6212e9bb57c219dc

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "./precache-manifest.56e555d7fd40f6f6bea073d862f38678.js"
+  "./precache-manifest.1cb879a85bd352b133c961dceb802ff4.js"
 );
 
 self.addEventListener('message', (event) => {
