0d4d31b01befdcd878b418159e54bef42b5e4954
Jason Smythe <jason@smythemail.za.net>
2020-05-11 20:13:05 +0200

    add working styled-components style creator

--- pages/_document.js
+++ pages/_document.js
@@ -1,11 +1,40 @@
 import Document, { Head, Main, NextScript } from "next/document";
 import { extractCritical } from "emotion-server";
+import { ServerStyleSheet } from "styled-components";
 
+// This file is a mix between : https://github.com/zeit/next.js/blob/master/examples/with-styled-components/pages/_document.js
+//                  and       : https://github.com/zeit/next.js/blob/master/examples/with-emotion/pages/_document.js
+//   I am a bit concerned that the performance of this isn't great because it has to render the page twice to get all the styles.
 export default class MyDocument extends Document {
-  static getInitialProps({ renderPage }) {
-    const page = renderPage();
+  static async getInitialProps(ctx) {
+    const page = ctx.renderPage();
     const styles = extractCritical(page.html);
-    return { ...page, ...styles };
+
+    const sheet = new ServerStyleSheet();
+    const originalRenderPage = ctx.renderPage;
+
+    try {
+      ctx.renderPage = () =>
+        originalRenderPage({
+          enhanceApp: (App) => (props) =>
+            sheet.collectStyles(<App {...props} />),
+        });
+
+      const initialProps = await Document.getInitialProps(ctx);
+      return {
+        ...page,
+        ...styles,
+        ...initialProps,
+        styles: (
+          <>
+            {initialProps.styles}
+            {sheet.getStyleElement()}
+          </>
+        ),
+      };
+    } finally {
+      sheet.seal();
+    }
   }
 
   render() {
